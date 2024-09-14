28b01ac57004b1bc61c22e4935684f60d79b6fe5
jspeeples15 <jspeeples15@gmail.com>
2024-09-13 20:27:37 -0400

    Ghost interaction w/ Embiggify spell

---
+++ Character/Scripts/player_main.gd
@@ -0,0 +1,59 @@
+extends CharacterBody2D
+
+@export var player_speed : int = 200
+
+var veloctiy : Vector2
+var direction : Vector2
+var is_interacting: bool
+
+signal interacting
+
+func _ready():
+	Dialogic.signal_event.connect(_on_dialogic_signal)
+
+func _physics_process(delta):
+	direction = Vector2.ZERO
+	direction.x = Input.get_axis("left", "right")
+	direction.y = Input.get_axis("up", "down")
+	
+	#movement basics
+	if direction:
+		direction = direction.normalized()
+		velocity = direction * player_speed
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, player_speed)
+		
+	move_and_slide()
+
+func _process(_delta):
+	if not is_interacting and Input.is_action_just_pressed("interact"):
+		interact()
+		
+func _on_dialogic_signal(msg: String):
+	if msg == "done":
+		is_interacting = false
+	elif msg == "wish_for_big":
+		print("making big!")
+		player_speed *= 2
+		scale *= 2
+
+func interact():
+	is_interacting = true
+	
+	var overlapping_bodies = $InteractionArea.get_overlapping_bodies()
+	var nearest_distance: float = $InteractionArea/InteractionRadius.shape.radius
+	var nearest_distance_squared = nearest_distance * nearest_distance
+	var interaction_choice = self
+	var min_length_away = nearest_distance_squared + 1
+	for overlapping_body in overlapping_bodies:
+		var body_distance_squared = self.global_position.distance_squared_to(overlapping_body.global_position)
+		if body_distance_squared < min_length_away:
+			interaction_choice = overlapping_body
+			min_length_away = body_distance_squared
+	
+	interacting.emit(interaction_choice.name)
+
+
+func _on_interacting(interaction_choice: String) -> void:
+	if interaction_choice == self.name:
+		Dialogic.start("idle_self")
---
+++ Environment/Scripts/test_prop.gd
@@ -0,0 +1,16 @@
+extends StaticBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_player_interacting(interaction_choice: String) -> void:
+	if interaction_choice == self.name:
+		Dialogic.start("welcome_ghost")
